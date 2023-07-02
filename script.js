c4e4e8e1b9ed985740b69540b411e0e9656b5bfc
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2023-07-02 11:44:57 -0300

    animações e a ativação de classe em tag html, experiência e educação.

--- script.js
+++ script.js
@@ -13,11 +13,62 @@ function escrevendoLetra() {
   ativaLetra(titulo);
 }
 
-const ativaMenu = document.querySelector('.fa-bars');
-const navMenu = document.querySelector("header .navegacao-primaria");
+function menuMobile() {
+  const ativaMenu = document.querySelector(".fa-bars");
+  const navMenu = document.querySelector("header .navegacao-primaria");
 
+  ativaMenu.addEventListener("click", () => {
+    ativaMenu.classList.toggle("fa-x");
+    navMenu.classList.toggle("ativado");
+  });
+}
 
-ativaMenu.addEventListener('click', () => {
-    ativaMenu.classList.toggle('fa-x');
-    navMenu.classList.toggle('ativado')
-})
+function sobreMim() {
+  const divExperiencia = document.querySelectorAll(".experience_content div");
+  const liExperiencia = document.querySelectorAll(".experience_content ul li");
+  const divEducation = document.querySelectorAll(".education_content div");
+  const liEducation = document.querySelectorAll(".education_content ul li");
+
+  divExperiencia[0].classList.add("ativo");
+  divEducation[0].classList.add("ativo");
+  liExperiencia[0].classList.add("ativo");
+  liEducation[0].classList.add("ativo");
+
+  function slideShowExperience(index) {
+    divExperiencia.forEach((div) => {
+      div.classList.remove("ativo");
+    });
+    liExperiencia.forEach((botao) => {
+      botao.classList.remove("ativo");
+    });
+    divExperiencia[index].classList.add("ativo");
+    liExperiencia[index].classList.add("ativo");
+  }
+
+  function slideShowEducation(index) {
+    divEducation.forEach((div) => {
+      div.classList.remove("ativo");
+    });
+    liEducation.forEach((botao) => {
+      botao.classList.remove("ativo");
+    });
+    divEducation[index].classList.add("ativo");
+    liEducation[index].classList.add("ativo");
+  }
+
+  liExperiencia.forEach((event, index) => {
+    event.addEventListener("click", () => {
+      slideShowExperience(index);
+    });
+  });
+
+  liEducation.forEach((event, index) => {
+    event.addEventListener("click", () => {
+      slideShowEducation(index);
+    });
+  });
+}
+
+sobreMim();
+menuMobile();
+escrevendoLetra();
